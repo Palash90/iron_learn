540aec35c256345dfaf19b25456d4cc13c9dd355
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-04-29 22:11:11 +0530

    Use of unwrap

--- src/tensor/add.rs
+++ src/tensor/add.rs
@@ -3,19 +3,19 @@ use crate::tensor::Tensor;
 use std::ops;
 
 impl<T: Numeric> ops::Add for Tensor<T> {
-    type Output = Result<Self, String>;
+    type Output = Result<Self, &'static str>;
 
-    fn add(self, rhs: Self) -> Result<Self, String> {
+    fn add(self, rhs: Self) -> Result<Self, &'static str> {
         let mut result_vector = Vec::with_capacity(self.data.len());
 
         if self.dimensions != rhs.dimensions {
             return Err(
-                "Incompatible Dimensions: Mismatch in dimension of two Tensors.".to_owned(),
+                "Incompatible Dimensions: Mismatch in dimension of two Tensors.",
             );
         }
 
         if self.data.len() != rhs.data.len() {
-            return Err("Element mismatch in two tensors.".to_owned());
+            return Err("Element mismatch in two tensors.");
         }
 
         for i in 0..self.data.len() {
--- tests/test.rs
+++ tests/test.rs
@@ -5,19 +5,19 @@ mod tests {
     pub fn add_i8() {
         let m1 = Tensor::<i8>::new(vec![1u32, 2u32], vec![1i8, 2i8]).unwrap();
         let m2 = Tensor::new(vec![1u32, 2u32], vec![3i8, 4i8]).unwrap();
-        let result = Tensor::new(vec![1u32, 2u32], vec![4i8, 6i8]);
+        let result = Tensor::new(vec![1u32, 2u32], vec![4i8, 6i8]).unwrap();
 
         println!("Hello Test");
 
-        assert_eq!(result, m1 + m2);
+        assert_eq!(result, (m1 + m2).unwrap());
     }
 
     #[test]
     pub fn add_i16() {
         let m1 = Tensor::new(vec![1u32, 2u32], vec![1i16, 2i16]).unwrap();
         let m2 = Tensor::new(vec![1u32, 2u32], vec![3i16, 4i16]).unwrap();
-        let result = Tensor::new(vec![1u32, 2u32], vec![4i16, 6i16]);
+        let result = Tensor::new(vec![1u32, 2u32], vec![4i16, 6i16]).unwrap();
 
-        assert_eq!(result, m1 + m2);
+        assert_eq!(result, (m1 + m2).unwrap());
     }
 }
