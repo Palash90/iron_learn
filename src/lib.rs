ee5244b173038113276206cf87cf4b3fddf1ba12
Palash Kanti Kundu <palash.kanti.kundu.90@gmail.com>
2024-05-14 04:33:55 +0530

    Version upgrade

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,13 @@
 //! A pure Rust Machine Learning Library
 //!
 //! ## Status
-//! Version 0.2.0 released with limited Matrix Manipulation abilities. Under active development for further implementation support.
+//! Version 0.3.0 released with limited Matrix Manipulation abilities. Under active development for further implementation support.
 //!
 //! ## Overview
-//! This library is designed to facilitate machine learning tasks with a focus on linear algebra operations. Currently, the library supports matrix addition, subtraction,  multiplication and transpose, providing a robust foundation for building more complex machine learning algorithms.
+//! This library is designed to facilitate machine learning tasks with a focus on linear algebra operations. 
+//! Currently, the library supports matrix addition, subtraction,  multiplication, 
+//! transpose, scaling by a scalar and a gradient descent function providing a robust foundation for building more complex 
+//! machine learning algorithms.
 //!
 
 mod complex;
